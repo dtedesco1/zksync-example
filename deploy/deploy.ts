5606fb27d92679a7de6633a53e2f0777f3735ee8
dtedesco1 <dtedesco1@gmail.com>
2022-07-23 14:59:08 +0800

    add deployment files

---
+++ deploy/deploy.ts
@@ -0,0 +1,36 @@
+import { utils, Wallet } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+import WALLET_PRIVATE_KEY from "../config";
+
+// An example of a deploy script that will deploy and call a simple contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the Greeter contract`);
+
+  // Initialize the wallet.
+  const wallet = new Wallet(WALLET_PRIVATE_KEY);
+
+  // Create deployer object and load the artifact of the contract we want to deploy.
+  const deployer = new Deployer(hre, wallet);
+  const artifact = await deployer.loadArtifact("Greeter");
+
+  // Deposit some funds to L2 in order to be able to perform L2 transactions.
+  const depositAmount = ethers.utils.parseEther("0.001");
+  const depositHandle = await deployer.zkWallet.deposit({
+    to: deployer.zkWallet.address,
+    token: utils.ETH_ADDRESS,
+    amount: depositAmount,
+  });
+  // Wait until the deposit is processed on zkSync
+  await depositHandle.wait();
+
+  // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  // `greeting` is an argument for contract constructor.
+  const greeting = "DFT says hello on zkSync!";
+  const greeterContract = await deployer.deploy(artifact, [greeting]);
+
+  // Show the contract info.
+  const contractAddress = greeterContract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+}
